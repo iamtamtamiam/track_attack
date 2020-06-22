282990861704860a7ba21817cb0bd6dd0fe613cb
iamtamtamiam <vutam_a@hotmail.com>
2020-06-22 12:45:31 -0500

    remove dev comments characteradapter

--- track_attack_frontend/src/charactersadapter.js
+++ track_attack_frontend/src/charactersadapter.js
@@ -5,31 +5,14 @@ class CharactersAdapter {
     }
 
     getAllCharacters(){
-        // fetch("http://localhost:3000/characters")
-        // .then(res => res.json())
         configAdapter.get(`/characters`)
         .then(function(json){
             json.data.forEach(character => {
                 let newCharacter = new Character(character)
-                //this.allCharactersArray.push(newCharacter)
                 newCharacter.renderCharacterCheckbox()
-            
-
-            //     let characterOption = `
-            //     <label>
-            //         <input type="checkbox" id="${character.id}-option" name="${character.id}">
-            //         ${character.description}
-            //     </label>
-            // `
-            // document.getElementById("create-game-characters").innerHTML += characterOption
             })
         })
     }
-
-
-
-
-
 
 
 }
