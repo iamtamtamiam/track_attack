477a6a1cb9d04be8d9e673db7b4609a5a6610aed
iamtamtamiam <vutam_a@hotmail.com>
2020-06-22 10:50:18 -0500

    remove console.logs gameadapter

--- track_attack_frontend/src/gameadapter.js
+++ track_attack_frontend/src/gameadapter.js
@@ -1,21 +1,16 @@
 class GameAdapter {
   
     getIdForRender(gameOptionId){
-       
         configAdapter.get(`/games/`+ `${gameOptionId}`)
         .then(gameJson => {
-            console.log(gameJson)
             let gameToRender = new Game(gameJson)
             gameToRender.renderGameDisplay()
         })
     }
 
 
-
     postGame(gameNameInput, userId){
         let gameData = {name: gameNameInput, user_id: userId}
-        console.log(gameData) //might have to parse int userId
-
     
         configAdapter.post(`/games`, configAdapter.setPostObj(gameData))
         .then(function(json) {
@@ -23,25 +18,13 @@ class GameAdapter {
                 alert(`Cannot create game board. ${Object.values(json.main)}`);
             }
             else {
-                console.log(json)
                 let newGame = new Game(json)
-                console.log(newGame)
-                
+
                 let grabbedHtml = document.querySelectorAll(`[id*="-option"]`)
                 newGame.selectionAdapter.getCheckedCharacters(newGame.id, grabbedHtml) //nodelist
-                
-                console.log(User.current)
-                
-               
-
+    
                 User.current.games.push(newGame)
-                
-                console.log(User.current.games)
                 User.current.getUserGames()
-                
-                
-        
-                
             }
 
         });
