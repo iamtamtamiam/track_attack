784c208299a1a8dd828f611436935a4e3447733e
iamtamtamiam <vutam_a@hotmail.com>
2020-06-22 12:08:25 -0500

    remove console.logs selectionadapter

--- track_attack_frontend/src/selectionadapter.js
+++ track_attack_frontend/src/selectionadapter.js
@@ -3,26 +3,17 @@ class SelectionAdapter {
     getCheckedCharacters(gameId, optionsNodeList){
         optionsNodeList.forEach(option => {
             if (option.checked === true){
-                console.log(option)
-            
-               
                 this.postSelection(gameId, option)
-        
             }
-
         })
     } 
 
 
     postSelection(gameId, option){
-        
         const selectionData = {game_id: gameId, character_id: option.name}
-        
-        console.log(selectionData)
         
         configAdapter.post(`/selections`, configAdapter.setPostObj(selectionData))
         .then(function(json) {
-            console.log(json)
             let newGameAdapter = new GameAdapter
             newGameAdapter.getIdForRender(json.game_id)
         })
